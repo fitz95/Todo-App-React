2c35745f7bfa853afa76fff7d6da889f43bbd961
ambrose-kibet <ambrosekibet36@gmail.com>
2023-01-26 15:28:54 +0300

    add  fix to test input

--- src/__test__/todotest.test.js
+++ src/__test__/todotest.test.js
@@ -12,13 +12,20 @@ const tasks = new TodoList();
 describe('add a task to list', () => {
   test("add a task 'Laundry' to the local storage and check if it exists on the local storage ", () => {
     localStorage.clear();
-    tasks.addBook('Laundry');
-    expect((localStorage).getItem('todoList')).toBe(JSON.stringify([{ description: 'Laundry', complete: false, index: 0 }]));
+    tasks.addBook('Do Laundry');
+    expect(localStorage.getItem('todoList')).toBe(
+      JSON.stringify([{ description: 'Do Laundry', complete: false, index: 0 }])
+    );
   });
 
   test("add a task 'Cooking' to the local storage and check if it exists on the local storage ", () => {
     tasks.addBook('Cooking');
-    expect((localStorage).getItem('todoList')).toBe(JSON.stringify([{ description: 'Laundry', complete: false, index: 0 }, { description: 'Cooking', complete: false, index: 1 }]));
+    expect(localStorage.getItem('todoList')).toBe(
+      JSON.stringify([
+        { description: 'Laundry', complete: false, index: 0 },
+        { description: 'Cooking', complete: false, index: 1 },
+      ])
+    );
   });
 });
 
@@ -26,5 +33,5 @@ describe('remove task with specific id from list', () => {
   localStorage.removeItem('todoList');
   tasks.addBook('Laundry');
   tasks.remove('0');
-  expect((localStorage).getItem('todoList')).toBe(JSON.stringify([]));
+  expect(localStorage.getItem('todoList')).toBe(JSON.stringify([]));
 });
